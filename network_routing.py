dced20adfcdae31b41d939bc77f655b4020e61b5
Josh Klingonsmith <jgklingo@byu.edu>
2024-10-11 15:15:36 -0600

    fix(passing tests with test implementation)

--- network_routing.py
+++ network_routing.py
@@ -2,16 +2,19 @@
 from heapq import *
 
 class TestMinPriorityQueue:
-    data = []
+    def __init__(self):
+        self.data = []
     def make_queue(self, dist):
         for key in dist.keys():
             heappush(self.data, (dist[key], key))
     def pop_min(self):
         return heappop(self.data)
     def update_key(self, vertex, value):
-        heappush(self.data, (value, vertex))
-    
-
+        # O(n) operation only for testing purposes
+        for i in range(len(self.data)):
+            if self.data[i][1] == vertex:
+                self.data[i] = (value, vertex)
+        heapify(self.data)
 
 ###########################################################
 
@@ -25,7 +28,8 @@ def dijkstra(graph: dict[int, dict[int, float]], start: int, heap_type: str) -> 
     if heap_type == "test":
         H = TestMinPriorityQueue()
     else:
-        raise ValueError(f"heap_type {heap_type} not recognized.")
+        raise NotImplementedError(f"No such heap type: {heap_type}")
+    H.make_queue(dist)
     while H.data:
         u = H.pop_min()[1]
         for v in graph[u].keys():
@@ -34,6 +38,16 @@ def dijkstra(graph: dict[int, dict[int, float]], start: int, heap_type: str) -> 
                 H.update_key(v, dist[v])
                 prev[v] = u
     return (dist, prev)
+
+
+def assemble_path(prev: dict[int, int], start: int, end: int) -> list[int]:
+    path = []
+    curr = end
+    while curr != start:
+        path.append(curr)
+        curr = prev[curr]
+    path.append(start)
+    return list(reversed(path))  # TODO: check time complexity
 
 
 def find_shortest_path_with_heap(
@@ -49,6 +63,9 @@ def find_shortest_path_with_heap(
         - the list of nodes (including `source` and `target`)
         - the cost of the path
     """
+    dist, prev = dijkstra(graph, source, "test")
+    return (assemble_path(prev, source, target), dist[target])
+
 
 
 def find_shortest_path_with_array(
@@ -64,3 +81,5 @@ def find_shortest_path_with_array(
         - the list of nodes (including `source` and `target`)
         - the cost of the path
     """
+    dist, prev = dijkstra(graph, source, "test")
+    return (assemble_path(prev, source, target), dist[target])
