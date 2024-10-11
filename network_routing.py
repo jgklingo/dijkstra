1d1000645592c0084a4a963b43aa2b4c24145a85
Josh Klingonsmith <jgklingo@byu.edu>
2024-10-11 14:14:58 -0600

    add(dijkstra and started testPQ)

--- network_routing.py
+++ network_routing.py
@@ -1,3 +1,41 @@
+# this code is only for testing, remove before submission #
+from heapq import *
+
+class TestMinPriorityQueue:
+    data = []
+    def make_queue(self, dist):
+        for key in dist.keys():
+            heappush(self.data, (dist[key], key))
+    def pop_min(self):
+        return heappop(self.data)
+    def update_key(self, vertex, value):
+        heappush(self.data, (value, vertex))
+    
+
+
+###########################################################
+
+def dijkstra(graph: dict[int, dict[int, float]], start: int, heap_type: str) -> dict[int, float]:
+    dist = {}
+    prev = {}
+    for vertex in graph.keys():
+        dist[vertex] = float('inf')
+        prev[vertex] = None
+    dist[start] = 0
+    if heap_type == "test":
+        H = TestMinPriorityQueue()
+    else:
+        raise ValueError(f"heap_type {heap_type} not recognized.")
+    while H.data:
+        u = H.pop_min()[1]
+        for v in graph[u].keys():
+            if dist[u] + graph[u][v] < dist[v]:
+                dist[v] = dist[u] + graph[u][v]
+                H.update_key(v, dist[v])
+                prev[v] = u
+    return (dist, prev)
+
+
 def find_shortest_path_with_heap(
         graph: dict[int, dict[int, float]],
         source: int,
